8ff1728126cec0cc7a07e74c85ff64d1800775f2
Karol Grzesznik <karol.grzesznik@gmail.com>
2021-11-24 22:25:25 +0100

    Some changes in models.py

--- models.py
+++ models.py
@@ -1,10 +1,6 @@
 import math 
 import matplotlib.pyplot as plt
 
-# R_value = 20
-# C_value = 800
-# L_value = 40
-
 class selectModel():
     def __init__(self):
         self.RLCVariables()
@@ -30,12 +26,21 @@ class selectModel():
         L_value = input()
         print("Podaj Pojemność:")
         C_value = input()
-        
+
+frequency_table = []
+impedance_table = []
+
+class chart():
+    def __init__(self):
+        plt.plot(frequency_table,impedance_table)
+        plt.xscale("log")
+        plt.grid(visible=True,axis='both', which='both')
+        plt.ylabel('Impedancja [Ω]')
+        plt.xlabel('Częstotliwość [Hz]')
+        plt.show()
 
 class models():
     def RLC_perfect_model():
-        frequency_table = []
-        impedance_table = []
         for f in range(10,50000,10):
             Omega = 2*math.pi*f
             Resistance = float(R_value)
@@ -45,9 +50,7 @@ class models():
             Impedance = math.sqrt((Resistance**2)+(Reactance**2))
             frequency_table.append(f)
             impedance_table.append(Impedance)
-        plt.plot(frequency_table,impedance_table)
-        plt.xscale("log")
-        plt.show()
+        chart()
 
 
 
